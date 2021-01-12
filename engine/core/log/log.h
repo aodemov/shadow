55ef8f027ea0fca17eca7900dc2e8ee9e0f722e4
SamHurwitz <13941786+thesamhurwitz@users.noreply.github.com>
2021-01-12 23:40:44 +0500

    feat: log class

---
+++ engine/core/log/log.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <memory>
+
+#include <spdlog/spdlog.h>
+
+namespace Shadow {
+class Log {
+public:
+    static void Init();
+
+    inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return coreLogger; };
+    inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return clientLogger; };
+
+private:
+    static std::shared_ptr<spdlog::logger> coreLogger;
+    static std::shared_ptr<spdlog::logger> clientLogger;
+};
+
+}
+
+#define SH_CORE_INFO(...) ::Shadow::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define SH_CORE_WARN(...) ::Shadow::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define SH_CORE_ERROR(...) ::Shadow::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define SH_CORE_TRACE(...) ::Shadow::Log::GetCoreLogger()->trace(__VA_ARGS__)
+
+#define SH_INFO(...) ::Shadow::Log::GetClientLogger()->info(__VA_ARGS__)
+#define SH_WARN(...) ::Shadow::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define SH_ERROR(...) ::Shadow::Log::GetClientLogger()->error(__VA_ARGS__)
+#define SH_TRACE(...) ::Shadow::Log::GetClientLogger()->trace(__VA_ARGS__)
+
