f03f5ec8778bae67e1d81a8eb439a96d2602f278
SamHurwitz <13941786+thesamhurwitz@users.noreply.github.com>
2021-01-14 03:12:00 +0500

    feat: add ApplicationEvents

---
+++ engine/runtime/events/application_events.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <sstream>
+
+#include "runtime/events/event.h"
+
+namespace Shadow {
+
+class WindowResizeEvent : public Event {
+public:
+    WindowResizeEvent(unsigned int width, unsigned int height)
+        : width(width), height(height) {}
+
+    unsigned int GetWidth() const { return width; }
+    unsigned int GetHeight() const { return height; }
+
+    const char* GetName() const override { return "WindowResizeEvent"; }
+    std::string ToString() const override {
+        std::stringstream ss;
+        ss << "WindowResizeEvent: " << width << "; " << height;
+        return ss.str();
+    }
+private:
+    unsigned int width, height;
+};
+
+class WindowCloseEvent : public Event {
+public:
+    WindowCloseEvent() = default;
+
+    const char* GetName() const override { return "WindowCloseEvent"; }
+};
+
+
+
+}
