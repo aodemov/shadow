ccb51a3e59a32674a64b40a69f52a3f958cff5be
SamHurwitz <13941786+thesamhurwitz@users.noreply.github.com>
2021-01-13 23:36:19 +0500

    feat: add lock guard to EventBus

--- engine/runtime/events/event_bus.h
+++ engine/runtime/events/event_bus.h
@@ -6,6 +6,7 @@
 #include <memory>
 #include <algorithm>
 #include <functional>
+#include <mutex>
 
 #include "handler_function.h"
 
@@ -17,39 +18,57 @@ public:
     ~EventBus();
 
     template<class T, class EventType>
-    void addListener(void (T::*memberFunction)(EventType const&), T* instance) {
-        listeners.insert(std::make_pair<std::type_index, std::shared_ptr<HandlerFunction<T, EventType>>>(
-                typeid(EventType),
-                std::make_shared<HandlerFunction<T, EventType>>
-                    (memberFunction, instance)
-            ));
-    }
+    void addListener(void (T::*memberFunction)(EventType const&), T* instance);
 
     template<class T, class EventType>
-    void removeListener(void (T::*memberFunction)(EventType const&), T* instance) {
-        auto handlerEntries = listeners.equal_range(typeid(EventType));
-
-        for (auto handlerEntry = handlerEntries.first; handlerEntry != handlerEntries.second; ++handlerEntry) {
-            auto const& handler = std::dynamic_pointer_cast<HandlerFunction<T, EventType>>(handlerEntry->second);
-            if (handler.get()->instance == instance && handler.get()->memberFunction == memberFunction) {
-                listeners.erase(handlerEntry);
-                return;
-            }
-        }
-    }
+    void removeListener(void (T::*memberFunction)(EventType const&), T* instance);
 
     template<typename EventType>
-    void emit(EventType const& event) {
-        auto handlerEntries = listeners.equal_range(typeid(EventType));
-
-        for (auto handlerEntry = handlerEntries.first; handlerEntry != handlerEntries.second; ++handlerEntry) {
-            auto const& handler = handlerEntry->second;
-            handler->invoke(event);
-        }
-    }
+    void emit(EventType const& event);
 
 private:
     std::multimap<std::type_index, std::shared_ptr<HandlerFunctionBase>> listeners;
+
+    std::mutex mutex;
 };
+
+
+template<class T, class EventType>
+void EventBus::addListener(void (T::*memberFunction)(EventType const&), T* instance) {
+    std::lock_guard<std::mutex> lock(mutex);
+
+    listeners.insert(std::make_pair<std::type_index, std::shared_ptr<HandlerFunction<T, EventType>>>(
+            typeid(EventType),
+            std::make_shared<HandlerFunction<T, EventType>>
+                    (memberFunction, instance)
+    ));
+}
+
+template<class T, class EventType>
+void EventBus::removeListener(void (T::*memberFunction)(EventType const&), T* instance) {
+    std::lock_guard<std::mutex> lock(mutex);
+
+    auto handlerEntries = listeners.equal_range(typeid(EventType));
+
+    for (auto handlerEntry = handlerEntries.first; handlerEntry != handlerEntries.second; ++handlerEntry) {
+        auto const& handler = std::dynamic_pointer_cast<HandlerFunction<T, EventType>>(handlerEntry->second);
+        if (handler.get()->instance == instance && handler.get()->memberFunction == memberFunction) {
+            listeners.erase(handlerEntry);
+            return;
+        }
+    }
+}
+
+template<typename EventType>
+void EventBus::emit(EventType const& event) {
+    std::lock_guard<std::mutex> lock(mutex);
+
+    auto handlerEntries = listeners.equal_range(typeid(EventType));
+
+    for (auto handlerEntry = handlerEntries.first; handlerEntry != handlerEntries.second; ++handlerEntry) {
+        auto const& handler = handlerEntry->second;
+        handler->invoke(event);
+    }
+}
 
 }
