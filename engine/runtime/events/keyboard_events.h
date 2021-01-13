6b74549195a008827b9cfce2cce27e1167de11b1
SamHurwitz <13941786+thesamhurwitz@users.noreply.github.com>
2021-01-14 03:12:10 +0500

    feat: add KeyboardEvents

---
+++ engine/runtime/events/keyboard_events.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "runtime/events/event.h"
+#include "core/input/key_codes.h"
+
+namespace Shadow {
+
+class KeyPressedEvent : public Event {
+public:
+    KeyPressedEvent(const KeyCode keyCode)
+        : keyCode(keyCode) {}
+
+    KeyCode GetKeyCode() const { return keyCode };
+
+    const char* GetName() const override { return "KeyPressedEvent"; }
+    std::string ToString() const override {
+        std::stringstream ss;
+        ss << "KeyPressedEvent: " << keyCode;
+        return ss.str();
+    }
+
+private:
+    uint16_t keyCode;
+};
+
+class KeyReleasedEvent : public Event {
+public:
+    KeyReleasedEvent(const KeyCode keyCode)
+            : keyCode(keyCode) {}
+
+    KeyCode GetKeyCode() const { return keyCode };
+
+    const char* GetName() const override { return "KeyReleasedEvent"; }
+    std::string ToString() const override {
+        std::stringstream ss;
+        ss << "KeyReleasedEvent: " << keyCode;
+        return ss.str();
+    }
+
+private:
+    uint16_t keyCode;
+};
+
+class KeyRepeatedEvent : public Event {
+public:
+    KeyRepeatedEvent(const KeyCode keyCode)
+            : keyCode(keyCode) {}
+
+    KeyCode GetKeyCode() const { return keyCode };
+
+    const char* GetName() const override { return "KeyRepeatedEvent"; }
+    std::string ToString() const override {
+        std::stringstream ss;
+        ss << "KeyRepeatedEvent: " << keyCode;
+        return ss.str();
+    }
+
+private:
+    uint16_t keyCode;
+};
+
+}
