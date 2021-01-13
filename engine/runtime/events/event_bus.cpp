53998504ee3ceec5691d680d662cd2aabc7744e7
SamHurwitz <13941786+thesamhurwitz@users.noreply.github.com>
2021-01-14 03:12:50 +0500

    chore: clean event_bus.cpp file

--- engine/runtime/events/event_bus.cpp
+++ engine/runtime/events/event_bus.cpp
@@ -4,14 +4,4 @@
 
 namespace Shadow {
 
-EventBus::EventBus() = default;
-EventBus::~EventBus() = default;
-
-//    template<class T, class EventType>
-//    void removeListener(void (T::*memberFunction)(EventType*), T* instance) {
-//        auto it = std::find_if(listeners.begin(), listeners.end(), [&](std::pair<std::type_index const&, std::shared_ptr<HandlerFunctionBase>> const&) {
-//
-//        });
-//    }
-
 }
