d845ee81acd65cfea783557e507fe1fb48cfb752
SamHurwitz <13941786+thesamhurwitz@users.noreply.github.com>
2021-01-14 16:37:34 +0500

    feat: add key modifiers enum

--- shadow/core/input/key_codes.h
+++ shadow/core/input/key_codes.h
@@ -161,4 +161,17 @@ enum : MouseCode {
 };
 }
 
+using KeyModifiers = uint16_t;
+namespace Modifiers {
+enum : uint16_t {
+    None     = 0,
+    Shift    = 1 << 0,
+    Control  = 1 << 1,
+    Alt      = 1 << 2,
+    Super    = 1 << 3,
+    CapsLock = 1 << 4,
+    NumLock  = 1 << 5
+};
+}
+
 }
