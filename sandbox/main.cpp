2cf87acc4758bdd851870bed4baa37af5c5b576e
SamHurwitz <13941786+thesamhurwitz@users.noreply.github.com>
2021-01-14 22:34:17 +0500

    fix: Sandbox

--- sandbox/main.cpp
+++ sandbox/main.cpp
@@ -5,21 +5,7 @@
 
 using namespace Shadow;
 
-class Sandbox : public Application {
-public:
-    Sandbox() {
-
-    }
-
-    ~Sandbox() {
-
-    }
-};
-
 int main() {
     Log::Init();
-    auto app = new Sandbox();
-    app->Init();
-    app->Run();
-
+    Application::Start();
 }
