5c7e1f9389c49dc32c8ba0796612edefee9d2e54
SamHurwitz <13941786+thesamhurwitz@users.noreply.github.com>
2021-01-28 19:02:28 +0500

    feat: vertex array

---
+++ shadow/renderer/vertex_array.cpp
@@ -0,0 +1,48 @@
+#include "vertex_array.h"
+
+#include <glad/gl.h>
+
+namespace Shadow {
+
+VertexArray::VertexArray() {
+    glCreateVertexArrays(1, &rendererId);
+}
+
+VertexArray::~VertexArray() {
+    glDeleteVertexArrays(1, &rendererId);
+};
+
+void VertexArray::Bind() const {
+    glBindVertexArray(rendererId);
+}
+
+void VertexArray::Unbind() const {
+    glBindVertexArray(0);
+}
+
+void VertexArray::AddVertexBuffer(const std::shared_ptr<VertexBuffer> &vertexBuffer) {
+    glBindVertexArray(rendererId);
+    vertexBuffer->Bind();
+    uint32_t index = 0;
+    const auto& layout = vertexBuffer->GetLayout();
+    for (const auto& element : layout) {
+        glEnableVertexArrayAttrib(rendererId, index);
+        glVertexAttribPointer(index,
+                              element.GetComponentCount(),
+                              ShaderDataTypeToOpenGLBaseType(element.Type),
+                              element.Normalized ? GL_TRUE : GL_FALSE,
+                              layout.GetStride(),
+                              (const void*)element.Offset);
+        ++index;
+    }
+
+    vertexBuffers.push_back(vertexBuffer);
+}
+
+void VertexArray::SetIndexBuffer(const std::shared_ptr<IndexBuffer> &indexBuf) {
+    glBindVertexArray(rendererId);
+    indexBuf->Bind();
+
+    indexBuffer = indexBuf;
+}
+}
