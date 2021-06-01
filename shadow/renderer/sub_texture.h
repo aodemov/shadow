0e80e3556a35c633953ca48e322466ee7d589cef
Sam Hurwitz <contact.samhurwitz@gmail.com>
2021-06-01 22:57:53 +0500

    feat: add rotation to subtexture

--- shadow/renderer/sub_texture.h
+++ shadow/renderer/sub_texture.h
@@ -7,13 +7,15 @@ class SubTexture {
 public:
     explicit SubTexture(Ref<Texture> texture);
     SubTexture(Ref<Texture> texture, const glm::vec4& texCoords);
-    SubTexture(Ref<Texture> texture, const glm::vec2& offset, const glm::vec2& size);
+    SubTexture(Ref<Texture> texture, const glm::vec2& offset, const glm::vec2& size, float rotation = 0.0f);
 
     Ref<Texture> GetTexture() const { return mTexture; }
     glm::vec4 const& GetTexCoords() const { return mTexCoords; }
+    float GetRotation() const { return mRotation; }
 
 private:
     Ref<Texture> mTexture;
     glm::vec4 mTexCoords;
+    float mRotation = 0.0f;
 };
 }
