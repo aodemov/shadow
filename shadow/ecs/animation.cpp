19ef31642455c7a0028f228dddb42d55063bdbc8
Sam Hurwitz <contact.samhurwitz@gmail.com>
2021-06-02 16:16:15 +0500

    feat: add animation class

--- shadow/ecs/animation.cpp
+++ shadow/ecs/animation.cpp
@@ -1,5 +1,29 @@
 #include "animation.h"
 
 namespace Shadow {
+Animation::Animation(Ref<Texture> const& texture, std::initializer_list<AnimationElement> frames) {
+    mFrames.reserve(frames.size());
+    for (auto& frame : frames) {
+        mFrames.push_back({SubTexture(texture, frame.offset, frame.size), frame.duration });
+    }
+}
 
+Animation::Animation(const Ref<Texture> &texture, float duration, std::initializer_list<AnimationTile> tiles) {
+    mFrames.reserve(tiles.size());
+
+    float frameDuration = duration / tiles.size();
+
+    for (auto& tile : tiles) {
+        mFrames.push_back({SubTexture(texture, tile.offset, tile.size), frameDuration });
+    }
+}
+
+void Animation::Update(float delta) {
+    currentTime += delta;
+    if (currentTime > mFrames[currentFrame].duration) {
+        currentTime -= mFrames[currentFrame].duration;
+        currentFrame++;
+        currentFrame %= mFrames.size();
+    }
+}
 }
--- shadow/ecs/animation.h
+++ shadow/ecs/animation.h
@@ -1,7 +1,34 @@
 #pragma once
 
+#include "shadow/renderer/sub_texture.h"
+
 namespace Shadow {
 class Animation {
+public:
+    struct AnimationTile {
+        glm::vec2 offset;
+        glm::vec2 size;
+    };
 
+    struct AnimationElement {
+        glm::vec2 offset;
+        glm::vec2 size;
+        float duration;
+    };
+
+    Animation(Ref<Texture> const& texture, std::initializer_list<AnimationElement> frames);
+    Animation(Ref<Texture> const& texture, float duration, std::initializer_list<AnimationTile> tiles);
+
+    void Update(float delta);
+    const SubTexture& GetTexture() { return mFrames[currentFrame].texture; }
+private:
+    struct AnimationFrame {
+        SubTexture texture;
+        float duration;
+    };
+
+    std::vector<AnimationFrame> mFrames;
+    float currentTime;
+    int currentFrame = 0;
 };
 }
