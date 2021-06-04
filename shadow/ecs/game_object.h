be6fbcb153e8e725889621a3586b79c0001627b6
Sam Hurwitz <contact.samhurwitz@gmail.com>
2021-06-04 18:39:41 +0500

    feat: add gameobject components

--- shadow/ecs/game_object.h
+++ shadow/ecs/game_object.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "scene.h"
+#include "component.h"
 
 namespace Shadow {
 class GameObject {
@@ -11,18 +12,23 @@ public:
     GameObject(const GameObject&) = default;
     GameObject& operator=(const GameObject&) = default;
 
-    virtual void OnLoad() {};
-    virtual void OnDestroy() {};
+    virtual void OnLoad() {}
+    virtual void OnDestroy() {}
 
-    virtual void FixedUpdate(float delta) {};
-    virtual void VariableUpdate(float delta) {};
+    virtual void FixedUpdate(float delta) {}
+    virtual void VariableUpdate(float delta) {}
 
-    virtual void OnEnable() {};
-    virtual void OnDisable() {};
+    virtual void OnEnable() {}
+    virtual void OnDisable() {}
 
-    glm::vec2 position { 0.0f, 0.0f };
-    glm::vec2 scale { 1.0f, 1.0f };
-    float rotation { 0.0f };
+    template<class T>
+    T& GetComponent() const;
+    template<class T, typename... TArgs>
+    void AddComponent(TArgs&&... args);
+    template<class T>
+    void RemoveComponent();
+    template<class T>
+    bool HasComponent();
 protected:
     template<class EventType>
     void On(EventBus::Function<EventType> function) {
@@ -31,5 +37,32 @@ protected:
 private:
     friend class Scene;
     Scene* mScene {nullptr};
+    std::map<Component::ComponentType, Scope<Component>> mComponents;
 };
+
+template<class T>
+T& GameObject::GetComponent() const {
+    return mComponents.at(Component::GetComponentType<T>());
+}
+
+template<class T, typename... TArgs>
+void GameObject::AddComponent(TArgs&&... args) {
+    Scope<T> component = MakeScope<T>(std::forward<TArgs>(args)...);
+    component->mGameObject = this;
+
+    component->OnLoad();
+
+    mComponents.emplace(Component::GetComponentType<T>(), std::move(component));
+}
+
+template<class T>
+void GameObject::RemoveComponent() {
+    mComponents.erase(Component::GetComponentType<T>());
+}
+
+template<class T>
+bool GameObject::HasComponent() {
+    return mComponents.contains(Component::GetComponentType<T>());
+}
+
 }
