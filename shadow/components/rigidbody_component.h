c935c5eac9063022b02f80ea37d33992a46da7a3
Sam Hurwitz <contact.samhurwitz@gmail.com>
2021-06-18 09:46:19 +0500

    feat: rigid body component

---
+++ shadow/components/rigidbody_component.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "shadow/ecs/component.h"
+
+namespace Shadow {
+class RigidbodyComponent : public Component {
+public:
+    RigidbodyComponent() = default;
+    RigidbodyComponent(const RigidbodyComponent&) = default;
+    explicit RigidbodyComponent(glm::vec2 velocity)
+    : Velocity(velocity) {}
+
+    glm::vec2 Velocity{0.0f};
+};
+}
