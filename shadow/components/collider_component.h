2602052ac7666ba41c6745c4a84573ee3474a472
Sam Hurwitz <contact.samhurwitz@gmail.com>
2021-06-18 09:46:12 +0500

    feat: collider component

---
+++ shadow/components/collider_component.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "shadow/ecs/component.h"
+
+namespace Shadow {
+class ColliderComponent : public Component {
+public:
+    ColliderComponent() = default;
+    ColliderComponent(const ColliderComponent&) = default;
+    explicit ColliderComponent(glm::vec4 box)
+    : Box(box) {}
+
+    glm::vec4 Box{0.0f};
+    bool IsTouching = false;
+};
+}
